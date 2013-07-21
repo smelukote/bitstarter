4ca78325a15a186de0f3db93519557b0aba8e772
Sudarshana Melukote <smelukote@gmail.com>
2013-07-21 16:25:55 +0000

    Udated web.js

--- web.js
+++ web.js
@@ -10,8 +10,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-fs.writeFile('index.html, 'Hello Node', function (err) {
-  if (err) throw err;
-  console.log('It\'s saved!');
-});
