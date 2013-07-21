19f834193e9824ab32f665fe327cfc75b5ba0c63
Sudarshana Melukote <smelukote@gmail.com>
2013-07-21 16:22:52 +0000

    Udated web.js

--- web.js
+++ web.js
@@ -3,10 +3,15 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('Hello World3!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+fs.writeFile('index.html, 'Hello Node', function (err) {
+  if (err) throw err;
+  console.log('It\'s saved!');
 });
